0dc3b4d7a178d48e12b38a405ad4650205c217cc
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 01:08:25 -0400

    why isn't this working????

--- api/index.py
+++ api/index.py
@@ -10,9 +10,11 @@ from ldclient.config import Config
 ## make sure LD is installed in your environment
 ### use pip install launchdarkly-server-sdk
 
+LD_KEY = os.environ.get('LD_SERVER_KEY')
+
 
 ## configure your LD SDK Key
-### ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
+ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
 
 ##  set your LD context
 ## context = 
