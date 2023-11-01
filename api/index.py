4e029484587af7bba595cf5d884f29ecedbde6e5
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 00:48:51 -0400

    comment out all the LD stuffs

--- api/index.py
+++ api/index.py
@@ -5,6 +5,20 @@ import os
 import uvicorn
 import pymysql
 
+## make sure LD is installed in your environment
+### pip install launchdarkly-server-sdk
+## from ldclient.config import Config
+
+## configure your LD SDK Key
+### ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
+
+##  set your LD context
+## context = 
+## Context.builder("context-key-123abc").name(Erin).build()
+
+## Evaluate your flag
+## val = client.variation('qrcode', context, False)
+
 app = FastAPI()
 
 @app.get("/api/python")
