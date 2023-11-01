833390f473dfb849675d2f051d1c40e05c0ef57e
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 01:56:41 -0400

    return and print don't do the same thing silly

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-LD_KEY = "654199bdc205c71290b98c92"
+LD_KEY = os.environ.get('LD_SERVER_KEY')
 
 # sample user for LD purposes - static user here for testing
 # user =  {
@@ -26,10 +26,10 @@ LD_KEY = "654199bdc205c71290b98c92"
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    print("the name's bond, james bond🍸")
 # here is where we'll launch some LD magic with our feature flags
 # your keys can be foudn on the Account settings page under the Environments tab for each project.
-    ldclient.set_config(Config("sdk-5709bc2e-ae00-450a-b3d8-3adf981ffd50"))
+    ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
     show_feature = ldclient.variation("earlyLaunch", user, False)
     if show_feature == True:
         return {"message": "Beta Feature is ON"}
