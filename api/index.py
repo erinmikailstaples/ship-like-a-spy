dd01d2f8202e63f282f9ef7a5a50122e3b59a09c
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 01:41:18 -0400

    add in beta user feedback flag

--- api/index.py
+++ api/index.py
@@ -1,10 +1,46 @@
 from fastapi import FastAPI
+# import launchdarkly_server_sdk as ldclient
+from ldclient import LDClient
+from ldclient.config import ldclient
+from ldclient.config import Config
+import os
+
+from fastapi import FastAPI
+
+# create the FastAPI app
 
 app = FastAPI()
+
+# sample user for LD purposes - static user here for testing
+user =  {
+    "key": "user-key-123abc",
+    "firstName": "James",
+    "lastName": "Bond",
+    "email": "spystuff007@example.com",
+    "custom": {
+        "groups": ["superspy"]
+    }
+}
+
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+# here is where we'll launch some LD magic with our feature flags
+# your keys can be foudn on the Account settings page under the Environments tab for each project.
+    ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
+    show_feature = ldclient.variation("earlyLaunch", user, False)
+    if show_feature == True:
+        return {"message": "Beta Feature is ON"}
+
+
+
+
+
+LD_KEY = os.environ.get('LD_SERVER_KEY')
+
+
+
 
 
 # # from fastapi import FastAPI
