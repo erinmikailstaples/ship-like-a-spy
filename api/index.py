418949e25a176a02ddf680f2ed6c5e411f5536c9
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 01:12:50 -0400

    back to hello world methinks

--- api/index.py
+++ api/index.py
@@ -1,27 +1,4 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Annotated
-import os
-import uvicorn
-import pymysql
-## from the launchdarkly sdk
-from ldclient.config import Config
-
-## make sure LD is installed in your environment
-### use pip install launchdarkly-server-sdk
-
-LD_KEY = os.environ.get('LD_SERVER_KEY')
-
-
-## configure your LD SDK Key
-ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
-
-##  set your LD context
-## context = 
-## Context.builder("context-key-123abc").name(Erin).build()
-
-## Evaluate your flag
-## val = client.variation('qrcode', context, False)
 
 app = FastAPI()
 
@@ -29,4 +6,32 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World"}
 
+app = FastAPI()
 
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}
+
+# # from fastapi import FastAPI
+# from pydantic import BaseModel
+# from typing import Annotated
+# import os
+# import uvicorn
+# import pymysql
+# ## from the launchdarkly sdk
+# from ldclient.config import Config
+
+# ## make sure LD is installed in your environment
+# ### use pip install launchdarkly-server-sdk
+
+# LD_KEY = os.environ.get('LD_SERVER_KEY')
+
+# ## configure your LD SDK Key
+# ldclient.set_config(Config(sdk_key="YOUR_SDK_KEY"))
+
+# ##  set your LD context
+# ## context = 
+# ## Context.builder("context-key-123abc").name(Erin).build()
+
+# ## Evaluate your flag
+# ## val = client.variation('qrcode', context, False)
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-         This is a tutorial that uses;
+          Get started by editing&nbsp;
           <Link href="/api/python">
-            <code className="font-mono font-bold">LaunchDarkly</code>
+            <code className="font-mono font-bold">api/index.py</code>
           </Link>
 
         </p>
