8bf965d541056dea271de5ba55d9c0058f832e55
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 00:37:06 -0400

    hello world FastAPI

--- api/index.py
+++ api/index.py
@@ -10,35 +10,3 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
-
-# Retrieve MySQL credentials from environment variables
-mysql_user = os.getenv("MYSQLUSER")
-mysql_password = os.getenv("MYSQLPASSWORD")
-mysql_host = os.getenv("MYSQLHOST")
-mysql_database = os.getenv("MYSQLDATABASE")
-mysql_port = os.getenv("MYSQLPORT")
-
-# Create a MySQL connection
-mysql_connection = pymysql.connect(
-    user=mysql_user,
-    password=mysql_password,
-    host=mysql_host,
-    database=mysql_database,
-    port=int(mysql_port)  # Ensure the port is an integer
-)
-
-app = FastAPI()
-
-@app.get("/")
-async def list_data():
-    cursor = mysql_connection.cursor(pymysql.cursors.DictCursor)
-    cursor.execute("SELECT * FROM bonddata")  # Replace 'bonddata' with your table name
-    data = cursor.fetchall()
-    cursor.close()
-
-    return {"data": data}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host=os.getenv("MYSQLHOST"), port=36985)
-
-
