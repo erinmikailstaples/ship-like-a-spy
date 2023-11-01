b9030ed23352af65100ac82ea04d1fbf6497d45e
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 00:31:13 -0400

    update and add database

--- api/index.py
+++ api/index.py
@@ -1,8 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Annotated
+import os
+import uvicorn
+import pymysql
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+from fastapi import FastAPI, HTTPException, Depends, status
+
+# Retrieve MySQL credentials from environment variables
+mysql_user = os.getenv("MYSQLUSER")
+mysql_password = os.getenv("MYSQLPASSWORD")
+mysql_host = os.getenv("MYSQLHOST")
+mysql_database = os.getenv("MYSQLDATABASE")
+mysql_port = os.getenv("MYSQLPORT")
+
+# Create a MySQL connection
+mysql_connection = pymysql.connect(
+    user=mysql_user,
+    password=mysql_password,
+    host=mysql_host,
+    database=mysql_database,
+    port=int(mysql_port)  # Ensure the port is an integer
+)
+
+app = FastAPI()
+
+@app.get("/")
+async def list_data():
+    cursor = mysql_connection.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM bonddata")  # Replace 'bonddata' with your table name
+    data = cursor.fetchall()
+    cursor.close()
+
+    return {"data": data}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv("MYSQLHOST"), port=36985)
+
 
