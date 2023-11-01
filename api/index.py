b1f779afe132e24c8a9aa0691bc66efb1053ff13
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2023-11-01 02:15:00 -0400

    testing fast API

--- api/index.py
+++ api/index.py
@@ -2,17 +2,10 @@ from fastapi import FastAPI
 # import launchdarkly_server_sdk as ldclient
 import ldclient
 from ldclient.config import Config
-import os
-
-from fastapi import FastAPI
 
 # create the FastAPI app
 
 app = FastAPI()
-
-from ldclient import LDClient
-
-ldclient = LDClient("sdk-5709bc2e-ae00-450a-b3d8-3adf981ffd50")
 
 # sample user for LD purposes - static user here for testing
 user =  {
@@ -25,15 +18,24 @@ user =  {
     }
 }
 
-
 @app.get("/api/python")
 def hello_world():
     print("the name's bond, james bond🍸")
-# here is where we'll launch some LD magic with our feature flags
-# your keys can be foudn on the Account settings page under the Environments tab for each project.
-    ldclient.set_config(Config("654199bdc205c71290b98c92"))
-    show_feature = ldclient.variation("earlyLaunch", user, False)
-    if show_feature:
-        print("earlyLaunch is ON!!!")
-    else:
-        print("earlyLaunch is OFF!!!")
+# # here is where we'll launch some LD magic with our feature flags
+# # your keys can be foudn on the Account settings page under the Environments tab for each project.
+#     ldclient.set_config(Config("sdk-5709bc2e-ae00-450a-b3d8-3adf981ffd5"))
+#     show_feature = ldclient.variation("earlyLaunch", user, False)
+#     if show_feature:
+#         print("earlyLaunch is ON!!!")
+#     else:
+#         print("earlyLaunch is OFF!!!")
+
+# ldclient.close()
+
+
+
+# ldclient = LDClient("0")
+
+
+
+
